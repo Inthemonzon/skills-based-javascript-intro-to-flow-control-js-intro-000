1f050bc2141d7004bb420dd509ec3c8e3e29106c
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-05 23:12:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,13 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19)
     return "You are a teenager!"
-} 
+}
 
 
 function teenager(age) {
   if (age >= 13 && age <= 19)
-  return "You are a teenager!"
   else "You are not a teenager"
-
+  return "You are a teenager!"
 }
 
 function ageChecker(age) {
